319545bf9321449d55212df4f4375a2582df934c
Mohammad-SJamal <mohammad.jamal@verizon.com>
2025-04-30 15:48:45 -0500

    changed index.html to unify bulb id names, and added the updating h3 and turning on ad off in script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your code here
+
+// let bulb1 = document.querySelector('#lightbulb1');
+// let bulb2 = document.querySelector("#lightbulb2");
+// let bulb3 = document.querySelector("#lightbulb3");
+
+let sub = document.querySelector("h3", ".subtitle");
+
+let count = 0;
+
+// bulb1.addEventListener("click", function(){
+//     bulb1.classList.toggle("active");
+// })
+
+const bulbs = document.querySelectorAll('#lightbulb')
+
+bulbs.forEach(bulb => {
+    bulb.addEventListener("click", () => {
+        bulb.classList.toggle("active");
+        count++;
+        sub.innerHTML = `You've clicked the lights ${count} times`;
+    })
+    
+})
